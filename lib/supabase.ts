3b1b14e762dd9920f6e25abb515cc25040715112
Lumina469 <lumima641@gmail.com>
2026-04-19 16:48:39 +0000

    Initial Lumina Links app

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://dawdtzqgwhqchjuursjj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGci0iJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhd2R0enFnd2hxY2hqdXJzamouZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5';
+
+export const supabase = createClient(SUPABASE_URL, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhd2R0enFnd2hxY2hqdXVyc2pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1ODE2MzAsImV4cCI6MjA5MjE1NzYzMH0.IFukRWG6r6oLRT4HdLBHVCk2q-kjtcXRB47ZxbXG1Zs);
+
